02ef26cf313e884e531702f2ee33fd1cb5b89ab4
Md. shofiqul islam <shofiqebr@gmail.com>
2024-11-23 06:36:14 +0600

    project setup and server setup

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.node }},
+  {
+    rules: {
+   
+      "no-unused-vars": "error",
+    
+    },
+
+  },
+  {
+    ignores: [".node_modules/* , .dist"]
+},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
+
