4fed7db65d534af1a7cf8335b9d6c862bcf00966
Md. shofiqul islam <shofiqebr@gmail.com>
2024-11-23 08:49:11 +0600

    add eslint and prettier

--- dist/app/config/index.js
+++ dist/app/config/index.js
@@ -6,3 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const path_1 = __importDefault(require("path"));
 dotenv_1.default.config({ path: path_1.default.join(process.cwd(), '.env') });
+exports.default = {
+    port: process.env.PORT,
+    database_url: process.env.DATABASE_URL
+};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,10 @@ export default [
     rules: {
    
       "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error"
     
     },
 
